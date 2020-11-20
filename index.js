c0c6cb10eb92a8034d9774c2e2bc1a09e316798d
Gizelle <cheerfulgelato@gmail.com>
2020-11-20 17:12:11 -0500

    20Nov2020 @17:15

--- index.js
+++ index.js
@@ -76,6 +76,60 @@ app.post("/signup", (req, res) => {
  
   });
 
+//2-CREATE ENDPOINT /login 
+app.post("/login", (req, res) => {
+
+    // 1-some console to check
+    console.log("1.0-Request to /login1 received");
+    console.log( "1.1-this is the body request (req.body) in JSON formatted string: " + req.body + ", typeof(req.body) is " + typeof req.body );
+    //(req.body) is {"username":"bob","password":"pwd123"}  
+    
+    // 2-retrieve the HTTP request body using the req.body expression
+    // PARSE the (req.body) JSON formatted string into JavaScript
+    let parsedBody = JSON.parse(req.body);
+    let result = parsedBody.username + " " + parsedBody.password;
+   
+    
+    //if user has signed up ie already in database? or I should call again the previous endpoint?
+    //res.send("you logged in as ", JSON.stringify({username: parsedBody.username, password: parsedBody.password}));
+  
+    let usr = parsedBody.username
+    console.log("usr = " + usr + ", does map has this usr?? " + users.has(usr) ) //username 
+    let actualUserPass = parsedBody.password//bob2131
+    console.log("actualUserPass = " + actualUserPass)//bob2131 
+    let expectedUserPass = users.get(usr)
+    console.log("expectedUserPass = " + expectedUserPass)//bob2131
+   //console.log("actualUser is " + actualUser + ", actualPass " + actualPass + ", expectedUser " + expectedUser)
+    
+      if (actualUserPass === expectedUserPass) {
+        console.log("checking if actualUserPass === expectedUserPass => " + actualUserPass + "===" + expectedUserPass)
+        let uniqueToken = genUniqueToken()
+        res.send(JSON.stringify({ success: true, token: uniqueToken }))
+        return
+      } else if (users.has(usr) && actualUserPass !== expectedUserPass ) {
+            console.log("usr = " + usr + ", does map has this usr?? " + users.has(usr) )
+            console.log("checking if actualUserPass ?== expectedUserPass => " + actualUserPass + "!==" + expectedUserPass)
+            res.send(JSON.stringify({ success: false, reason: "Invalid password" }))
+      }else if (usr=== '' || usr=== null || usr === undefined) {
+      console.log("parsedBody.username is " + parsedBody.username);
+      res.send(      JSON.stringify({ success: false, reason: "username field missing" }));
+      } else if (actualUserPass === "" || actualUserPass === undefined || actualUserPass === null) {//undefined
+      res.send(
+        JSON.stringify({ success: false, reason: "password field missing" }));
+    } 
+    
+    else
+    res.send(JSON.stringify({ success: false, reason: "User does not exist" }))
+  });
+  
+  //   // 3-CREATE ENDPOINT path: /create-channel  method: POST 
+// app.post("/create-channel", (req, res) => {}
+//  channelName 
+// // if success
+// // if token header missing
+// // if token is invalid
+// // if the channelName property is missing from the request body
+// // If a channel with the same name already exists
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
